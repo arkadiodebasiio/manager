d853a7593193c3f99385b1c1ef253a48119314b2
arkadiodebasiio <nomatter84@atomicmail.io>
2026-08-15 13:04:40 +0200

    Add isComboExecuting to boxerRed properties
    Added isComboExecuting property to boxerRed.

--- boxer-stats.js
+++ boxer-stats.js
@@ -9,7 +9,7 @@ export const chosenOrbitSpeed = strongHand === 'left' ? 0.023 : -0.023;
 export const boxerRed = {
     angle: Math.PI / 2, 
     orbitSpeed: chosenOrbitSpeed, 
-    radius: 18, // PRZYWRÓCONE: Oryginalny, mniejszy rozmiar ludzika
+    radius: 18, 
     color: '#e74c3c', 
     number: '1',
     animTimer: 0, 
@@ -30,13 +30,14 @@ export const boxerRed = {
     currentHand: 'left',
     isChargingSuper: false,
     superChargeFrames: 0,
-    superPunchTimer: 2700
+    superPunchTimer: 2700,
+    isComboExecuting: false
 };
 
 export const boxerBlue = { 
     x: 250, 
     y: 250, 
-    radius: 18, // PRZYWRÓCONE: Oryginalny, mniejszy rozmiar ludzika
+    radius: 18, 
     color: '#2980b9', 
     number: '2', 
     animTimer: 0, 
