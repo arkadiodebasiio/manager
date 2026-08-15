75235a2a781c045587686fcd161faae910468c1a
arkadiodebasiio <nomatter84@atomicmail.io>
2026-08-15 14:30:47 +0200

    Add files via upload

---
+++ renderer.js
@@ -0,0 +1,33 @@
+import { canvas, ctx, updatePhysics, boxerRed } from './engine.js';
+import { drawBlueBoxer, drawRedBoxer, drawBlockShield } from './renderer-hands.js';
+
+// Zabezpieczenie przed błędem braku zdefiniowanej ręki w pierwszej klatce gry
+if (!window.currentActivePunchHand) {
+    window.currentActivePunchHand = 'left';
+}
+
+function drawRing() {
+    ctx.fillStyle = '#d4ac0d'; ctx.fillRect(50, 50, 400, 400);
+    ctx.strokeStyle = '#b7950b'; ctx.lineWidth = 2;
+    for (let i = 70; i < 450; i += 40) { ctx.beginPath(); ctx.moveTo(i, 50); ctx.lineTo(i, 450); ctx.stroke(); }
+    for (let offset = 10; offset <= 30; offset += 10) {
+        ctx.strokeStyle = offset === 10 ? '#fff' : (offset === 20 ? '#c0392b' : '#2980b9');
+        ctx.lineWidth = 3; ctx.strokeRect(50 - offset/2, 50 - offset/2, 400 + offset, 400 + offset);
+    }
+    [{x: 50, y: 50, color: '#c0392b'}, {x: 450, y: 50, color: '#b91d29'}, {x: 50, y: 450, color: '#fff'}, {x: 450, y: 450, color: '#fff'}].forEach(c => {
+        ctx.beginPath(); ctx.arc(c.x, c.y, 8, 0, Math.PI * 2); ctx.fillStyle = c.color; ctx.fill();
+        ctx.strokeStyle = '#000'; ctx.lineWidth = 2; ctx.stroke();
+    });
+}
+
+function loop() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    drawRing(); 
+    updatePhysics(); 
+    drawBlueBoxer(); 
+    drawRedBoxer();
+    drawBlockShield(); 
+    requestAnimationFrame(loop);
+}
+
+loop();
